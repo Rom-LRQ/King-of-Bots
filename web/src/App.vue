499e3af1c65d550840f0968c96d5b496499cee13
Liu RQ <2806649895@qq.com>
2022-08-09 15:11:39 +0800

    搭建骨架

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="fontc" >Bot昵称：{{bot_name}}</div>
+  <div class="fontc" >Bot战力：{{bot_rating}}</div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name: "app",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getinfo/",
+      type: "get",
+      success: resp => {
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    })
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  body {
+    background-image: url(https://img.zcool.cn/community/01274855f7a84832f875a132a15b65.jpg?x-oss-process=image/auto-orient,1);
+    background-size: cover;
+  }
+  .fontc {
+    color: aqua;
+  }
 </style>
